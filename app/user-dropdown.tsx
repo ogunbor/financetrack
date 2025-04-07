68c03e5f119d696055f6aed30891518a67d1d807
mompei <ogunborjerry@gmail.com>
2025-04-07 12:58:33 +0100

    Route protection and added dashboard link to dropdown of clerk

--- app/user-dropdown.tsx
+++ app/user-dropdown.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { ChartColumnBigIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function UserDropdown() {
+    const router = useRouter();
     return (
         <UserButton
             showName
@@ -13,5 +16,16 @@ export default function UserDropdown() {
                     },
                 },
             }}
-        />);
+        >
+            <UserButton.MenuItems>
+                <UserButton.Action
+                    label="Dashboard"
+                    labelIcon={<ChartColumnBigIcon size={16} />}
+                    onClick={() => {
+                        router.push("/dashboard");
+                    }}
+                />
+            </UserButton.MenuItems>
+        </UserButton>
+    );
 }
