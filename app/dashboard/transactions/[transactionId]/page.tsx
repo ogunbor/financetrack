3093f6b380d71f37bd6e29bdabb4c52056f1a167
mompei <ogunborjerry@gmail.com>
2025-04-14 13:54:52 +0100

    Edit transaction page and render the priliminary transaction form

---
+++ app/dashboard/transactions/[transactionId]/page.tsx
@@ -0,0 +1,52 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getCategories } from "@/data/getCategories";
+import Link from "next/link";
+
+import { notFound } from "next/navigation";
+
+export default async function EditTransactionPage({
+    params,
+}: {
+    params: Promise<{ transactionId: string }>;
+}) {
+    const paramsValues = await params;
+    const transactionId = Number(paramsValues.transactionId);
+
+    if (isNaN(transactionId)) {
+        notFound();
+    }
+    const categories = await getCategories();
+    return (
+        <div className="max-w-screen-xl mx-auto py-10">
+
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/dashboard">Dashboard</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/dashboard/transactions">Transactions</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>New Transaction</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <Card className="mt-4 max-w-screen-md">
+                <CardHeader>
+                    <CardTitle>Edit Transaction</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    edit transaction
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
