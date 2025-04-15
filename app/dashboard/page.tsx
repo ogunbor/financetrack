81ee987ee16721f0a3df9c9ea5d90d8c9a8874ce
mompei <ogunborjerry@gmail.com>
2025-04-15 11:47:09 +0100

    Add the recent transactions table on the dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+import RecentTransactions from "./recent-transactions";
+
+export default async function DashboardPage() {
+    return (
+        <div className="max-w-screen-xl mx-auto py-5">
+            <h1 className="text-4xl font-semibold pb-5">Dashboard</h1>
+            <RecentTransactions />
+        </div>
+    );
+}
