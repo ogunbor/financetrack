201962b6380b38b256e4cf960b5b6c5cfdedcb52
mompei <ogunborjerry@gmail.com>
2025-04-16 01:50:34 +0100

    Add the cashflow card and dyanmic loading data

--- app/dashboard/cashflow.tsx
+++ app/dashboard/cashflow.tsx
@@ -1,9 +1,23 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getAnnualCashflow } from "@/data/getAnnualCashflow";
+import CashflowFilters from "./cashflow-filters";
+import { getTransactionYearsRange } from "@/data/getTransactionYearsRange";
 
 
-export default async function Cashflow() {
-    const cashflow = await getAnnualCashflow(2025)
+export default async function Cashflow({ year }: { year: number }) {
+    const [cashflow, yearsRange] = await Promise.all([
+        getAnnualCashflow(year),
+        getTransactionYearsRange(),
+    ]);
+
     return (
-        <div />
+        <Card className="mb-5">
+            <CardHeader>
+                <CardTitle className="flex justify-between">
+                    <span>Cashflow</span>
+                    <CashflowFilters year={year} yearsRange={yearsRange} />
+                </CardTitle>
+            </CardHeader>
+        </Card>
     );
 }
